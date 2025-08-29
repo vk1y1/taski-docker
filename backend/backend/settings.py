2d7185dd52d9eb6f7602f4bc1a31993d5a96ff58
Vadim Korneev <vk1y1@yandex.ru>
2025-08-29 16:23:19 +0300

    Add settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
-    ''
+    'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 )
 
 DEBUG = (os.getenv('DEBUG') == 'True')
